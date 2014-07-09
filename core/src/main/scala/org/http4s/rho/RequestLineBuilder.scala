3d42fdc66d83acb0cb0a004a28216d1ec2d976ef
André Rouél <andre.rouel@gmail.com>
2014-07-09 20:54:43 +0200

    Formatted source code

--- core/src/main/scala/org/http4s/rho/RequestLineBuilder.scala
+++ core/src/main/scala/org/http4s/rho/RequestLineBuilder.scala
@@ -1,21 +1,18 @@
 package org.http4s
 package rho
 
-import bits.PathAST.PathRule
-import org.http4s.rho.bits.QueryAST.{QueryAnd, TypedQuery, QueryRule}
-import bits.HeaderAST.{EmptyHeaderRule, HeaderRule}
+import org.http4s.rho.bits.QueryAST.{ QueryAnd, TypedQuery, QueryRule }
 import org.http4s.rho.bits.UriConverter
 
+import bits.HeaderAST.{ EmptyHeaderRule, HeaderRule }
+import bits.PathAST.PathRule
 import shapeless.HList
 import shapeless.ops.hlist.Prepend
 
-import scala.util.Try
-
-
 case class RequestLineBuilder[T <: HList](path: PathRule, query: QueryRule)
   extends TypedBuilder[T]
-  with UriConvertible
-{
+  with UriConvertible {
+
   override def validators: HeaderRule = EmptyHeaderRule
 
   def &[T1 <: HList](q: TypedQuery[T1])(implicit prep: Prepend[T1, T]): RequestLineBuilder[prep.Out] =
@@ -26,4 +23,5 @@ case class RequestLineBuilder[T <: HList](path: PathRule, query: QueryRule)
       p <- UriConverter.createPath(path)
       q <- UriConverter.createQuery(query)
     } yield UriTemplate(path = p, query = Some(q))
+
 }
