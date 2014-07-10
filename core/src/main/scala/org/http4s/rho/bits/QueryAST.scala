649caa4ee09802e56d451123500eaf258bce4f98
André Rouél <andre.rouel@gmail.com>
2014-07-10 19:59:17 +0200

    Added method to TypedQuery to resolve names to capture

--- core/src/main/scala/org/http4s/rho/bits/QueryAST.scala
+++ core/src/main/scala/org/http4s/rho/bits/QueryAST.scala
@@ -19,6 +19,11 @@ object QueryAST {
 
     final def &&[T1 <: HList](v: TypedQuery[T1])(implicit prepend: Prepend[T, T1]): TypedQuery[prepend.Out] = and(v)
 
+    /**
+     * Resolves names of query parameters to capture
+     */
+    lazy val names: List[String] = collectNames(rule)
+
     override def asUriTemplate = for (q <- UriConverter.createQuery(rule)) yield UriTemplate(query = Some(q))
   }
 
@@ -33,4 +38,18 @@ object QueryAST {
   case class MetaCons(query: QueryRule, meta: Metadata) extends QueryRule
 
   case object EmptyQuery extends QueryRule
+
+  private def collectNames(rule: QueryRule): List[String] = {
+    @scala.annotation.tailrec
+    def go(r: List[QueryRule], acc: List[String]): List[String] = r match {
+      case Nil => acc
+      case MetaCons(query, meta) :: rs => go(rs, acc)
+      case QueryAnd(a, b) :: rs => go(a :: b :: rs, acc)
+      case QueryCapture(name, p, default, m) :: rs => go(rs, name :: acc)
+      case QueryOr(a, b) :: rs => go(a :: rs, acc)
+      case EmptyQuery :: rs => go(rs, acc)
+    }
+    go(List(rule), Nil).reverse
+  }
+
 }
