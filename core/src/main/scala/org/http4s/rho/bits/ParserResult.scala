22a5589819d7997f99c928d7cf7ccf162eba510f
Bryce Anderson <bryce.anderson22@gmail.com>
2014-07-16 11:07:53 -0400

    Fix problem with naming of ParseResult Success
    Conflicts with the Try Success so the name was changed

--- core/src/main/scala/org/http4s/rho/bits/ParserResult.scala
+++ core/src/main/scala/org/http4s/rho/bits/ParserResult.scala
@@ -3,18 +3,18 @@ package org.http4s.rho.bits
 
 sealed trait ParserResult[+T] {
   def map[T2](f: T => T2): ParserResult[T2] = this match {
-    case Success(v)              => Success(f(v))
+    case ParseSuccess(v)              => ParseSuccess(f(v))
     case e@ ParseFailure(_)      => e
     case e@ ValidationFailure(_) => e
   }
 
   def flatMap[T2](f: T => ParserResult[T2]): ParserResult[T2] = this match {
-    case Success(v)              => f(v)
+    case ParseSuccess(v)              => f(v)
     case e@ ParseFailure(_)      => e
     case e@ ValidationFailure(_) => e
   }
 }
 
-case class Success[+T](result: T) extends ParserResult[T]
+case class ParseSuccess[+T](result: T) extends ParserResult[T]
 case class ParseFailure(reason: String) extends ParserResult[Nothing]
 case class ValidationFailure(reason: String) extends ParserResult[Nothing]
--- core/src/test/scala/org/http4s/rho/bits/ParserResultSpec.scala
+++ core/src/test/scala/org/http4s/rho/bits/ParserResultSpec.scala
@@ -8,7 +8,7 @@ class ParserResultSpec extends Specification {
   "ParserResult" should {
 
     "map a Success" in {
-      Success(3).map(_.toString) should_== Success("3")
+      ParseSuccess(3).map(_.toString) should_== ParseSuccess("3")
     }
 
     "map a ValidationFailure" in {
@@ -22,33 +22,33 @@ class ParserResultSpec extends Specification {
     }
 
     "flatMap a Success" in {
-      Success(3).flatMap(i => Success(i.toString)) should_== Success("3")
+      ParseSuccess(3).flatMap(i => ParseSuccess(i.toString)) should_== ParseSuccess("3")
     }
 
     "flatMap a ValidationFailure" in {
       val result: ParserResult[Int] = ValidationFailure("foo")
-      result.flatMap(i => Success(i.toString)) should_== result
+      result.flatMap(i => ParseSuccess(i.toString)) should_== result
     }
 
     "flatMap a ParseFailure" in {
       val result: ParserResult[Int] = ParseFailure("foo")
-      result.flatMap(i => Success(i.toString)) should_== result
+      result.flatMap(i => ParseSuccess(i.toString)) should_== result
     }
 
     "work in a for comprehensions" in {
-      val a = Success("foo")
-      val b = Success("bar")
+      val a = ParseSuccess("foo")
+      val b = ParseSuccess("bar")
       val result = for {
         foo <- a
         bar <- b
       } yield (foo + bar)
 
-      result should_== Success("foobar")
+      result should_== ParseSuccess("foobar")
     }
 
     "work in a failing for comprehensions" in {
       val a: ParserResult[String] = ParseFailure("boo")
-      val b: ParserResult[String] = Success("bar")
+      val b: ParserResult[String] = ParseSuccess("bar")
       val result1 = for {
         foo <- a
         bar <- b
