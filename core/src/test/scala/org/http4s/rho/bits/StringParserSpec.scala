679cafcac8da1f60825ad995c2074ff6f7627d3a
André Rouél <andre.rouel@googlemail.com>
2014-08-11 20:56:17 +0200

    Added more StringParser extensions to be able to implicitly convert basic data types

---
+++ core/src/test/scala/org/http4s/rho/bits/StringParserSpec.scala
@@ -0,0 +1,56 @@
+package org.http4s.rho.bits
+
+import org.specs2.mutable.Specification
+
+class StringParserSpec extends Specification {
+
+  "StringParser" should {
+
+    "parse true" in {
+      new BooleanParser().parse("true") == ParserSuccess(true)
+    }
+    "parse false" in {
+      new BooleanParser().parse("false") == ParserSuccess(false)
+    }
+    "not parse non-boolean" in {
+      new BooleanParser().parse("unknown") must haveClass[ParserFailure]
+    }
+
+    "parse double" in {
+      new DoubleParser().parse("1.1") == ParserSuccess[Double](1.1)
+    }
+    "parse non-double" in {
+      new DoubleParser().parse("unknown") must haveClass[ParserFailure]
+    }
+
+    "parse float" in {
+      new FloatParser().parse("1.1") == ParserSuccess[Float](1.1f)
+    }
+    "parse non-float" in {
+      new FloatParser().parse("unknown") must haveClass[ParserFailure]
+    }
+
+    "parse int" in {
+      new IntParser().parse("1") == ParserSuccess[Int](1)
+    }
+    "parse non-int" in {
+      new IntParser().parse("12345678901") must haveClass[ParserFailure]
+    }
+
+    "parse long" in {
+      new LongParser().parse("1") == ParserSuccess[Long](1)
+    }
+    "parse non-long" in {
+      new LongParser().parse("12345678901234567890") must haveClass[ParserFailure]
+    }
+
+    "parse short" in {
+      new ShortParser().parse("1") == ParserSuccess[Short](1)
+    }
+    "parse non-short" in {
+      new ShortParser().parse("12345678") must haveClass[ParserFailure]
+    }
+
+  }
+
+}
