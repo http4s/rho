3cd1ecc76dc6d9a679a880c10d6479ac2d38a972
Bryce Anderson <bryce.anderson22@gmail.com>
2014-08-27 05:30:30 -0400

    Complete Status code implementations
    * Add all codes to the dictionary in ResultInfo
    * Add correct types to all ResponseGenerators

--- core/src/main/scala/org/http4s/rho/bits/ResponseGenerator.scala
+++ core/src/main/scala/org/http4s/rho/bits/ResponseGenerator.scala
@@ -40,128 +40,128 @@ object ResponseGeneratorInstances extends ResponseGeneratorInstances
 
 trait ResponseGeneratorInstances {
 
-//  object Continue extends EmptyResponseGenerator(Status.Continue)
-//
-//  // TODO support Upgrade header
+  object Continue extends EmptyResponseGenerator[Status.Continue.type](Status.Continue)
+
+  // TODO support Upgrade header
   object SwitchingProtocols extends EmptyResponseGenerator[Status.Ok.type](Status.SwitchingProtocols)
 
   object Ok extends EntityResponseGenerator[Status.Ok.type](Status.Ok)
 
-//  object Created extends EntityResponseGenerator(Status.Created)
-//
-//  object Accepted extends EntityResponseGenerator(Status.Accepted)
-//
-//  object NonAuthoritativeInformation extends EntityResponseGenerator(Status.NonAuthoritativeInformation)
-//
-//  object NoContent extends EmptyResponseGenerator(Status.NoContent)
-//
-//  object ResetContent extends EmptyResponseGenerator(Status.ResetContent)
-//
-//  // TODO helpers for Content-Range and multipart/byteranges
-//  object PartialContent extends EntityResponseGenerator(Status.PartialContent)
-//
-//  object MultiStatus extends EntityResponseGenerator(Status.MultiStatus)
-//
-//  object AlreadyReported extends EntityResponseGenerator(Status.AlreadyReported)
-//
-//  object IMUsed extends EntityResponseGenerator(Status.IMUsed)
-//
-//  object MultipleChoices extends LocationResponseGenerator(Status.MultipleChoices)
-//
+  object Created extends EntityResponseGenerator[Status.Created.type](Status.Created)
+
+  object Accepted extends EntityResponseGenerator[Status.Accepted.type](Status.Accepted)
+
+  object NonAuthoritativeInformation extends EntityResponseGenerator[Status.NonAuthoritativeInformation.type](Status.NonAuthoritativeInformation)
+
+  object NoContent extends EmptyResponseGenerator[Status.NoContent.type](Status.NoContent)
+
+  object ResetContent extends EmptyResponseGenerator[Status.ResetContent.type](Status.ResetContent)
+
+  // TODO helpers for Content-Range and multipart/byteranges
+  object PartialContent extends EntityResponseGenerator[Status.PartialContent.type](Status.PartialContent)
+
+  object MultiStatus extends EntityResponseGenerator[Status.MultiStatus.type](Status.MultiStatus)
+
+  object AlreadyReported extends EntityResponseGenerator[Status.AlreadyReported.type](Status.AlreadyReported)
+
+  object IMUsed extends EntityResponseGenerator[Status.IMUsed.type](Status.IMUsed)
+
+  object MultipleChoices extends LocationResponseGenerator[Status.MultipleChoices.type](Status.MultipleChoices)
+
   object MovedPermanently extends LocationResponseGenerator[Status.MovedPermanently.type](Status.MovedPermanently)
-//
-//  object Found extends LocationResponseGenerator(Status.Found)
-//
-//  object SeeOther extends LocationResponseGenerator(Status.SeeOther)
-//
-//  object NotModified extends EntityResponseGenerator(Status.NotModified)
-//
-//  // Note: UseProxy is deprecated in RFC7231, so we will not ease its creation here.
-//
-//  object TemporaryRedirect extends LocationResponseGenerator(Status.TemporaryRedirect)
-//
-//  object PermanentRedirect extends LocationResponseGenerator(Status.PermanentRedirect)
-//
-//  object BadRequest extends EntityResponseGenerator(Status.BadRequest)
-//
-//  object Unauthorized extends ResponseGenerator {
-//    val status: Status = Status.Unauthorized
-//    def apply(challenge: Challenge, challenges: Challenge*): Task[Result[EmptyResult]] =
-//      Task.now(Result(Response(status).putHeaders(Header.`WWW-Authenticate`(challenge, challenges: _*))))
-//  }
-//
-//  object PaymentRequired extends EntityResponseGenerator(Status.PaymentRequired)
-//
-//  object Forbidden extends EntityResponseGenerator(Status.Forbidden)
-//
+
+  object Found extends LocationResponseGenerator[Status.Found.type](Status.Found)
+
+  object SeeOther extends LocationResponseGenerator[Status.SeeOther.type](Status.SeeOther)
+
+  object NotModified extends EntityResponseGenerator[Status.NotModified.type](Status.NotModified)
+
+  // Note: UseProxy is deprecated in RFC7231, so we will not ease its creation here.
+
+  object TemporaryRedirect extends LocationResponseGenerator[Status.TemporaryRedirect.type](Status.TemporaryRedirect)
+
+  object PermanentRedirect extends LocationResponseGenerator[Status.PermanentRedirect.type](Status.PermanentRedirect)
+
+  object BadRequest extends EntityResponseGenerator[Status.BadRequest.type](Status.BadRequest)
+
+  object Unauthorized extends ResponseGenerator {
+    val status: Status = Status.Unauthorized
+    def apply(challenge: Challenge, challenges: Challenge*): Task[Result[Status.Unauthorized.type, EmptyResult]] =
+      Task.now(Result(Response(status).putHeaders(Header.`WWW-Authenticate`(challenge, challenges: _*))))
+  }
+
+  object PaymentRequired extends EntityResponseGenerator[Status.PaymentRequired.type](Status.PaymentRequired)
+
+  object Forbidden extends EntityResponseGenerator[Status.Forbidden.type](Status.Forbidden)
+
   object NotFound extends EntityResponseGenerator[Status.NotFound.type](Status.NotFound)
-//
-//  object MethodNotAllowed extends ResponseGenerator {
-//    val status: Status = Status.MethodNotAllowed
-//    def apply(method: Method*): Task[Result[EmptyResult]] = Task.now {
-//      Result(Response(status).putHeaders(Header.Raw("Allow".ci, method.mkString(","))))
-//    }
-//  }
-//
-//  object NotAcceptable extends EntityResponseGenerator(Status.NotAcceptable)
-//
-//  object ProxyAuthenticationRequired extends EntityResponseGenerator (Status.ProxyAuthenticationRequired)
-//
-//  // TODO send Connection: close?
-//  object RequestTimeout extends EntityResponseGenerator(Status.RequestTimeout)
-//
-//  object Conflict extends EntityResponseGenerator(Status.Conflict)
-//
-//  object Gone extends EntityResponseGenerator(Status.Gone)
-//
-//  object LengthRequired extends EntityResponseGenerator(Status.LengthRequired)
-//
-//  object PreconditionFailed extends EntityResponseGenerator(Status.PreconditionFailed)
-//
-//  object PayloadTooLarge extends EntityResponseGenerator(Status.PayloadTooLarge)
-//
-//  object UriTooLong extends EntityResponseGenerator(Status.UriTooLong)
-//
-//  object UnsupportedMediaType extends EntityResponseGenerator(Status.UnsupportedMediaType)
-//
-//  object RangeNotSatisfiable extends EntityResponseGenerator(Status.RangeNotSatisfiable)
-//
-//  object ExpectationFailed extends EntityResponseGenerator(Status.ExpectationFailed)
-//
-//  object UnprocessableEntity extends EntityResponseGenerator(Status.UnprocessableEntity)
-//
-//  object Locked extends EntityResponseGenerator(Status.Locked)
-//
-//  object FailedDependency extends EntityResponseGenerator(Status.FailedDependency)
-//
-//  // TODO Mandatory upgrade field
-//  object UpgradeRequired extends EntityResponseGenerator(Status.UpgradeRequired)
-//
-//  object PreconditionRequired extends EntityResponseGenerator(Status.PreconditionRequired)
-//
-//  object TooManyRequests extends EntityResponseGenerator(Status.TooManyRequests)
-//
-//  object RequestHeaderFieldsTooLarge extends EntityResponseGenerator(Status.RequestHeaderFieldsTooLarge)
-//
-//  object InternalServerError extends EntityResponseGenerator(Status.InternalServerError)
-//
-//  object NotImplemented extends EntityResponseGenerator(Status.NotImplemented)
-//
-//  object BadGateway extends EntityResponseGenerator(Status.BadGateway)
-//
-//  object ServiceUnavailable extends EntityResponseGenerator(Status.ServiceUnavailable)
-//
-//  object GatewayTimeout extends EntityResponseGenerator(Status.GatewayTimeout)
-//
-//  object HttpVersionNotSupported extends EntityResponseGenerator(Status.HttpVersionNotSupported)
-//
-//  object VariantAlsoNegotiates extends EntityResponseGenerator (Status.VariantAlsoNegotiates)
-//
-//  object InsufficientStorage extends EntityResponseGenerator(Status.InsufficientStorage)
-//
-//  object LoopDetected extends EntityResponseGenerator(Status.LoopDetected)
-//
-//  object NotExtended extends EntityResponseGenerator(Status.NotExtended)
-//
-//  object NetworkAuthenticationRequired extends EntityResponseGenerator(Status.NetworkAuthenticationRequired)
+
+  object MethodNotAllowed extends ResponseGenerator {
+    val status: Status = Status.MethodNotAllowed
+    def apply(method: Method*): Task[Result[Status.MethodNotAllowed.type, EmptyResult]] = Task.now {
+      Result(Response(status).putHeaders(Header.Raw("Allow".ci, method.mkString(","))))
+    }
+  }
+
+  object NotAcceptable extends EntityResponseGenerator[Status.NotAcceptable.type](Status.NotAcceptable)
+
+  object ProxyAuthenticationRequired extends EntityResponseGenerator[Status.ProxyAuthenticationRequired.type](Status.ProxyAuthenticationRequired)
+
+  // TODO send Connection: close?
+  object RequestTimeout extends EntityResponseGenerator[Status.RequestTimeout.type](Status.RequestTimeout)
+
+  object Conflict extends EntityResponseGenerator[Status.Conflict.type](Status.Conflict)
+
+  object Gone extends EntityResponseGenerator[Status.Gone.type](Status.Gone)
+
+  object LengthRequired extends EntityResponseGenerator[Status.LengthRequired.type](Status.LengthRequired)
+
+  object PreconditionFailed extends EntityResponseGenerator[Status.PreconditionFailed.type](Status.PreconditionFailed)
+
+  object PayloadTooLarge extends EntityResponseGenerator[Status.PayloadTooLarge.type](Status.PayloadTooLarge)
+
+  object UriTooLong extends EntityResponseGenerator[Status.UriTooLong.type](Status.UriTooLong)
+
+  object UnsupportedMediaType extends EntityResponseGenerator[Status.UnsupportedMediaType.type](Status.UnsupportedMediaType)
+
+  object RangeNotSatisfiable extends EntityResponseGenerator[Status.RangeNotSatisfiable.type](Status.RangeNotSatisfiable)
+
+  object ExpectationFailed extends EntityResponseGenerator[Status.ExpectationFailed.type](Status.ExpectationFailed)
+
+  object UnprocessableEntity extends EntityResponseGenerator[Status.UnprocessableEntity.type](Status.UnprocessableEntity)
+
+  object Locked extends EntityResponseGenerator[Status.Locked.type](Status.Locked)
+
+  object FailedDependency extends EntityResponseGenerator[Status.FailedDependency.type](Status.FailedDependency)
+
+  // TODO Mandatory upgrade field
+  object UpgradeRequired extends EntityResponseGenerator[Status.UpgradeRequired.type](Status.UpgradeRequired)
+
+  object PreconditionRequired extends EntityResponseGenerator[Status.PreconditionFailed.type](Status.PreconditionRequired)
+
+  object TooManyRequests extends EntityResponseGenerator[Status.TooManyRequests.type](Status.TooManyRequests)
+
+  object RequestHeaderFieldsTooLarge extends EntityResponseGenerator[Status.RequestHeaderFieldsTooLarge.type](Status.RequestHeaderFieldsTooLarge)
+
+  object InternalServerError extends EntityResponseGenerator[Status.InternalServerError.type](Status.InternalServerError)
+
+  object NotImplemented extends EntityResponseGenerator[Status.NotImplemented.type](Status.NotImplemented)
+
+  object BadGateway extends EntityResponseGenerator[Status.BadGateway.type](Status.BadGateway)
+
+  object ServiceUnavailable extends EntityResponseGenerator[Status.ServiceUnavailable.type](Status.ServiceUnavailable)
+
+  object GatewayTimeout extends EntityResponseGenerator[Status.GatewayTimeout.type](Status.GatewayTimeout)
+
+  object HttpVersionNotSupported extends EntityResponseGenerator[Status.HttpVersionNotSupported.type](Status.HttpVersionNotSupported)
+
+  object VariantAlsoNegotiates extends EntityResponseGenerator[Status.VariantAlsoNegotiates.type](Status.VariantAlsoNegotiates)
+
+  object InsufficientStorage extends EntityResponseGenerator[Status.InsufficientStorage.type](Status.InsufficientStorage)
+
+  object LoopDetected extends EntityResponseGenerator[Status.LoopDetected.type](Status.LoopDetected)
+
+  object NotExtended extends EntityResponseGenerator[Status.NotExtended.type](Status.NotExtended)
+
+  object NetworkAuthenticationRequired extends EntityResponseGenerator[Status.NetworkAuthenticationRequired.type](Status.NetworkAuthenticationRequired)
 }
--- core/src/main/scala/org/http4s/rho/bits/ResultInfo.scala
+++ core/src/main/scala/org/http4s/rho/bits/ResultInfo.scala
@@ -1,7 +1,7 @@
 package org.http4s.rho.bits
 
 import org.http4s.Status
-import scala.reflect.runtime.universe.{ Type, TypeTag, typeOf }
+import scala.reflect.runtime.universe.{ Type, typeOf }
 
 
 sealed trait ResultInfo
@@ -11,11 +11,63 @@ case class StatusOnly(status: Status) extends ResultInfo
 case object Empty extends ResultInfo
 
 object ResultInfo {
-
-  // TODO: Finish this map!
   private lazy val statusIndex: Set[(Type, Status)] = Set(
+    (typeOf[Status.NotExtended.type], Status.NotExtended),
+    (typeOf[Status.NetworkAuthenticationRequired.type], Status.NetworkAuthenticationRequired),
+    (typeOf[Status.LoopDetected.type], Status.LoopDetected),
+    (typeOf[Status.InsufficientStorage.type], Status.InsufficientStorage),
+    (typeOf[Status.VariantAlsoNegotiates.type], Status.VariantAlsoNegotiates),
+    (typeOf[Status.HttpVersionNotSupported.type], Status.HttpVersionNotSupported),
+    (typeOf[Status.GatewayTimeout.type], Status.GatewayTimeout),
+    (typeOf[Status.ServiceUnavailable.type], Status.ServiceUnavailable),
+    (typeOf[Status.BadGateway.type], Status.BadGateway),
+    (typeOf[Status.NotImplemented.type], Status.NotImplemented),
+    (typeOf[Status.Locked.type], Status.Locked),
+    (typeOf[Status.InternalServerError.type], Status.InternalServerError),
+    (typeOf[Status.RequestHeaderFieldsTooLarge.type], Status.RequestHeaderFieldsTooLarge),
+    (typeOf[Status.TooManyRequests.type], Status.TooManyRequests),
+    (typeOf[Status.PreconditionRequired.type], Status.PreconditionRequired),
+    (typeOf[Status.UpgradeRequired.type], Status.UpgradeRequired),
+    (typeOf[Status.FailedDependency.type], Status.FailedDependency),
+    (typeOf[Status.UnprocessableEntity.type], Status.UnprocessableEntity),
+    (typeOf[Status.ExpectationFailed.type], Status.ExpectationFailed),
+    (typeOf[Status.RangeNotSatisfiable.type], Status.RangeNotSatisfiable),
+    (typeOf[Status.UnsupportedMediaType.type], Status.UnsupportedMediaType),
+    (typeOf[Status.UriTooLong.type], Status.UriTooLong),
+    (typeOf[Status.PayloadTooLarge.type], Status.PayloadTooLarge),
+    (typeOf[Status.PreconditionFailed.type], Status.PreconditionFailed),
+    (typeOf[Status.LengthRequired.type], Status.LengthRequired),
+    (typeOf[Status.Gone.type], Status.Gone),
+    (typeOf[Status.Conflict.type], Status.Conflict),
+    (typeOf[Status.RequestTimeout.type], Status.RequestTimeout),
+    (typeOf[Status.ProxyAuthenticationRequired.type], Status.ProxyAuthenticationRequired),
+    (typeOf[Status.NotAcceptable.type], Status.NotAcceptable),
+    (typeOf[Status.MethodNotAllowed.type], Status.MethodNotAllowed),
+    (typeOf[Status.NotFound.type], Status.NotFound),
+    (typeOf[Status.Forbidden.type], Status.Forbidden),
+    (typeOf[Status.PaymentRequired.type], Status.PaymentRequired),
+    (typeOf[Status.Unauthorized.type], Status.Unauthorized),
+    (typeOf[Status.BadRequest.type], Status.BadRequest),
+    (typeOf[Status.PermanentRedirect.type], Status.PermanentRedirect),
+    (typeOf[Status.TemporaryRedirect.type], Status.TemporaryRedirect),
+    (typeOf[Status.UseProxy.type], Status.UseProxy),
+    (typeOf[Status.NotModified.type], Status.NotModified),
+    (typeOf[Status.SeeOther.type], Status.SeeOther),
+    (typeOf[Status.MovedPermanently.type], Status.MovedPermanently),
+    (typeOf[Status.MultipleChoices.type], Status.MultipleChoices),
+    (typeOf[Status.IMUsed.type], Status.IMUsed),
+    (typeOf[Status.AlreadyReported.type], Status.AlreadyReported),
+    (typeOf[Status.MultiStatus.type], Status.MultiStatus),
+    (typeOf[Status.PartialContent.type], Status.PartialContent),
+    (typeOf[Status.ResetContent.type], Status.ResetContent),
+    (typeOf[Status.NoContent.type], Status.NoContent),
+    (typeOf[Status.NonAuthoritativeInformation.type], Status.NonAuthoritativeInformation),
+    (typeOf[Status.Accepted.type], Status.Accepted),
+    (typeOf[Status.Created.type], Status.Created),
     (typeOf[Status.Ok.type], Status.Ok),
-    (typeOf[Status.NotFound.type], Status.NotFound)
+    (typeOf[Status.Processing.type], Status.Processing),
+    (typeOf[Status.SwitchingProtocols.type], Status.SwitchingProtocols),
+    (typeOf[Status.Continue.type], Status.Continue)
   )
   
   def getStatus(tpe: Type): Option[Status] = statusIndex.find{ case (t2, s) => tpe =:= t2 }.map(_._2)
