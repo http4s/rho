454e0403c17f0719e77370784ee99d70a27d0b88
André Rouél <andre.rouel@gmail.com>
2014-11-18 17:36:03 +0100

    Added test case for 'SwaggerFormats.withSerializers'

--- swagger/src/test/scala/org/http4s/rho/swagger/SwaggerFormatsSpec.scala
+++ swagger/src/test/scala/org/http4s/rho/swagger/SwaggerFormatsSpec.scala
@@ -1,14 +1,12 @@
 package org.http4s.rho.swagger
 
-import scala.collection.mutable
+import scala.collection.mutable.LinkedHashMap
 import scala.reflect.runtime.universe._
 
 import org.specs2.mutable.Specification
 
 import com.wordnik.swagger.model.Model
 import com.wordnik.swagger.model.ModelProperty
-
-
 
 class SwaggerFormatsSpec extends Specification {
 
@@ -24,13 +22,21 @@ class SwaggerFormatsSpec extends Specification {
 
   "SwaggerFormats" should {
 
+    "withSerializers" in {
+      val swaggerFormats = DefaultSwaggerFormats.withSerializers(typeOf[FruitBox], Set(Model("fruit-box", "FruitBox", "model.FruitBox", LinkedHashMap.empty[String, ModelProperty])))
+      def models[T](t: TypeTag[T]): Set[Model] = TypeBuilder.collectModels(t.tpe, Set.empty, swaggerFormats)
+      models(typeTag[FruitBox]).nonEmpty must_== true
+      models(typeTag[FruitBox]).head.id must_== "fruit-box"
+      models(typeTag[FruitBox]).head.name must_== "FruitBox"
+      models(typeTag[FruitBox]).head.qualifiedType must_== "model.FruitBox"
+      models(typeTag[FruitBox]).head.properties.isEmpty must_== true
+    }
+
     "withFieldSerializers" in {
       val swaggerFormats = DefaultSwaggerFormats.withFieldSerializers(typeOf[Seq[Fruit]], ModelProperty("List", "array«Fruit»"))
       def models[T](t: TypeTag[T]): Set[Model] = TypeBuilder.collectModels(t.tpe, Set.empty, swaggerFormats)
       models(typeTag[FruitBox]).nonEmpty must_== true
       models(typeTag[FruitBox]).head.properties.head._1 must_== "fruits"
-
-      // following assertion fails
       models(typeTag[FruitBox]).head.properties.head._2 must_== ModelProperty("List", "array«Fruit»")
     }
   }
