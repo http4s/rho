14a1bfb503784ffc79f7ba90ceac914b5df30eb2
Carlos Encarnacion <encarnacion.carlos@gmail.com>
2015-02-24 11:49:07 -0400

    Bring `SwaggerSupportSpec` up to date

--- swagger/src/test/scala/org/http4s/rho/swagger/SwaggerSupportSpec.scala
+++ swagger/src/test/scala/org/http4s/rho/swagger/SwaggerSupportSpec.scala
@@ -3,9 +3,13 @@ package rho
 package swagger
 
 import org.specs2.mutable.Specification
+
 import org.http4s.rho.bits.MethodAliases.GET
 
 class SwaggerSupportSpec extends Specification with RequestRunner {
+
+  import org.json4s.JsonAST._
+  import org.json4s.jackson._
 
   lazy val service = new SwaggerSupport {
     GET / "hello" |>> { () => Ok("hello world") }
@@ -16,13 +20,15 @@ class SwaggerSupportSpec extends Specification with RequestRunner {
 
     "Expose an API listing" in {
       val r = Request(GET, Uri(path = "/swagger.json"))
-      //val JObject(List((_, JString(a)), (_, JString(b)))) = parseJson(checkOk(r)) \\ "path"
-      //Set(a, b) should_== Set("/swagger.json", "/hello")
-      pending
+
+      val JObject(List((a, JObject(_)), (b, JObject(_)), (c, JObject(_)))) =
+        parseJson(checkOk(r)) \\ "paths"
+
+      Set(a, b, c) should_== Set("/swagger.json", "/hello", "/hello/{string}")
     }
 
     "Expose an API description" in {
-      val r = Request(GET, Uri(path = "/swagger.json/hello"))
+      //val r = Request(GET, Uri(path = "/swagger.json/hello"))
       //val json = parseJson(checkOk(r))
 
       //val JArray(List(a, b)) = json \ "apis"
@@ -30,10 +36,10 @@ class SwaggerSupportSpec extends Specification with RequestRunner {
       //(a \ "path" should_== JString("/hello")) &&
       //(b \ "path" should_== JString("/hello/{string}"))
       pending
-    }
+     }
 
     "Return a 404 on bad path" in {
-      val r = Request(GET, Uri(path = "/swagger.json/notfound"))
+      //val r = Request(GET, Uri(path = "/swagger.json/notfound"))
       //checkStatus(r)(_ == Status.NotFound) should_== "Api Not Found: swagger.json/notfound"
       pending
     }
