35f6fdb7e64ff1b68c462a51fbbe9de1ed8ed707
Bryce Anderson <bryce.anderson22@gmail.com>
2015-08-18 08:08:57 -0400

    Make the examples work again
    This includes a bit of a 'shim' that should be a part of http4s. The orElse functionality is reintroduced with a syntax class that checks for 404 NotFound responses.

--- examples/src/main/scala/com/http4s/rho/swagger/demo/Main.scala
+++ examples/src/main/scala/com/http4s/rho/swagger/demo/Main.scala
@@ -1,11 +1,11 @@
 package com.http4s.rho.swagger.demo
 
 import org.http4s.server.blaze.BlazeBuilder
+import com.http4s.rho.Helpers._
 
 object Main extends App {
   BlazeBuilder
-    .mountService(StaticContentService.routes)
-    .mountService(MyService.toService)
+    .mountService(StaticContentService.routes orElse MyService.toService)
     .bindLocal(8080)
     .start
     .run
