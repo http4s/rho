685c670934db1a31959d59f3d81dfb165d4d7e6a
sheng.mba <shengcer@gmail.com>
2015-08-19 22:53:53 -0400

    to address #99

--- core/src/main/scala/org/http4s/rho/ExecutableCompiler.scala
+++ core/src/main/scala/org/http4s/rho/ExecutableCompiler.scala
@@ -24,43 +24,50 @@ trait ExecutableCompiler {
   def runValidation(req: Request, v: HeaderRule, stack: HList): ResultResponse[HList] = {
     import bits.HeaderAST.MetaCons
     v match {
-      case HeaderAnd(a, b) => runValidation(req, a, stack).flatMap(runValidation(req, b, _))
+      case HeaderAnd(a, b) ⇒ runValidation(req, a, stack).flatMap(runValidation(req, b, _))
 
-      case HeaderOr(a, b) => runValidation(req, a, stack).orElse(runValidation(req, b, stack))
+      case HeaderOr(a, b) ⇒ runValidation(req, a, stack).orElse(runValidation(req, b, stack))
 
-      case HeaderExists(key, f) => req.headers.get(key) match {
-        case Some(h) => f(h).fold[ResultResponse[HList]](SuccessResponse(stack))(f => FailureResponse.result(f))
-        case None => FailureResponse.badRequest(s"Missing header: ${key.name}")
+      case HeaderExists(key, f) ⇒ req.headers.get(key) match {
+        case Some(h) ⇒ 
+          scalaz.\/.fromTryCatchNonFatal( f(h) ) match {
+            case scalaz.-\/(t) ⇒ FailureResponse.badRequest(t.getMessage())
+            case scalaz.\/-(option) ⇒ option.fold[ResultResponse[HList]](SuccessResponse(stack))(f ⇒ FailureResponse.result(f))
+          }
+        case None ⇒ FailureResponse.badRequest(s"Missing header: ${key.name}")
       }
 
-      case HeaderCapture(key, f, default) => req.headers.get(key) match {
-        case Some(h) =>
-          f(h).fold(f => FailureResponse.result(f), r => SuccessResponse(r::stack))
+      case HeaderCapture(key, f, default) ⇒ req.headers.get(key) match {
+        case Some(h) ⇒
+          scalaz.\/.fromTryCatchNonFatal( f(h) ) match {
+            case scalaz.-\/(t) ⇒ FailureResponse.badRequest(t.getMessage())
+            case scalaz.\/-(option) ⇒ option.fold(f ⇒ FailureResponse.result(f), r ⇒ SuccessResponse(r::stack))
+          }
 
-        case None => default match {
-          case Some(r) => FailureResponse.result(r)
-          case None    => FailureResponse.badRequest(s"Missing header: ${key.name}")
+        case None ⇒ default match {
+          case Some(r) ⇒ FailureResponse.result(r)
+          case None    ⇒ FailureResponse.badRequest(s"Missing header: ${key.name}")
         }
       }
 
-      case MetaCons(r, _) => runValidation(req, r, stack)
+      case MetaCons(r, _) ⇒ runValidation(req, r, stack)
 
-      case EmptyHeaderRule => SuccessResponse(stack)
+      case EmptyHeaderRule ⇒ SuccessResponse(stack)
     }
   }
 
   def runQuery(req: Request, v: QueryRule, stack: HList): ResultResponse[HList] = {
     import QueryAST.MetaCons
     v match {
-      case QueryAnd(a, b) => runQuery(req, a, stack).flatMap(runQuery(req, b, _))
+      case QueryAnd(a, b) ⇒ runQuery(req, a, stack).flatMap(runQuery(req, b, _))
 
-      case QueryOr(a, b) => runQuery(req, a, stack).orElse(runQuery(req, b, stack))
+      case QueryOr(a, b) ⇒ runQuery(req, a, stack).orElse(runQuery(req, b, stack))
 
-      case QueryCapture(name, parser, default, _) => parser.collect(name, req.multiParams, default).map(_ :: stack)
+      case QueryCapture(name, parser, default, _) ⇒ parser.collect(name, req.multiParams, default).map(_ :: stack)
 
-      case MetaCons(r, _) => runQuery(req, r, stack)
+      case MetaCons(r, _) ⇒ runQuery(req, r, stack)
 
-      case EmptyQuery => SuccessResponse(stack)
+      case EmptyQuery ⇒ SuccessResponse(stack)
     }
   }
 }
