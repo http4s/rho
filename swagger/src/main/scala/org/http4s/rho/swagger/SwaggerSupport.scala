9742c84ff2641663d7ce91b545375c9428db3977
Bryce Anderson <bryce.anderson22@gmail.com>
2015-10-01 08:43:12 -0400

    Swagger `apiPath` is now `TypedPath[HNil]`
    This allows for changing the directory etc.
    
    closes #95

--- swagger/src/main/scala/org/http4s/rho/swagger/SwaggerSupport.scala
+++ swagger/src/main/scala/org/http4s/rho/swagger/SwaggerSupport.scala
@@ -5,8 +5,9 @@ package swagger
 import io.swagger.util.Json
 
 import headers.`Content-Type`
+import org.http4s.rho.bits.PathAST.TypedPath
 
-import shapeless.HList
+import shapeless.{HNil, HList}
 
 trait SwaggerSupport extends RhoService {
   import models._
@@ -17,12 +18,12 @@ trait SwaggerSupport extends RhoService {
   /** Override the `swaggerFormats` to add your own custom serializers */
   def swaggerFormats: SwaggerFormats = DefaultSwaggerFormats
 
-  def apiPath = "swagger.json"
+  def apiPath: TypedPath[HNil] = "swagger.json"
 
   def apiInfo: Info = Info(title = "My API", version = "1.0.0")
 
   "Swagger documentation" **
-    GET / apiPath |>> { () => Ok(swaggerResponse).withHeaders(`Content-Type`(MediaType.`application/json`)) }
+    GET / apiPath |>> { () => Ok(swaggerResponse).putHeaders(`Content-Type`(MediaType.`application/json`)) }
 
   override protected def append[T <: HList](rr: RhoRoute[T]): Unit = {
     super.append(rr)
