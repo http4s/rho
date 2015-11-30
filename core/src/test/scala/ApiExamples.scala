075a2194326b593e970a70aa48fdded63bc74fd8
Bryce Anderson <bryce.anderson22@gmail.com>
2015-11-30 08:56:39 -0500

    Move ApiExamples and shorten lines to ~80 chars
    After ~80 chars they result in a horizontal scroll bar in the wiki page.

--- core/src/test/scala/ApiExamples.scala
+++ core/src/test/scala/ApiExamples.scala
@@ -1,15 +1,15 @@
-package org.http4s
-package rhotest
 
-import org.http4s.websocket.WebsocketBits.WebSocketFrame
-import org.specs2.mutable.Specification
+import java.util.concurrent.atomic.AtomicInteger
+
+import org.http4s.headers.{ETag, `Content-Length`}
 import org.http4s.rho._
+import org.http4s.server.websocket._
+import org.http4s.websocket.WebsocketBits.WebSocketFrame
+import org.http4s.{Request, UrlForm}
+import org.specs2.mutable.Specification
+
 import scalaz.concurrent.Task
-
-import org.http4s.headers.{`Content-Length`, ETag}
-import server.websocket.WS
-
-
+import scalaz.stream
 
 
 class ApiExamples extends Specification {
@@ -36,30 +36,38 @@ class ApiExamples extends Specification {
       /// src_inlined PathCapture
       new RhoService {
         // Use combinators to parse and capture path parameters
-        GET / "helloworldnumber" / pathVar[Int] / "foo" |>> { i: Int => Ok("Received $i") }
+        GET / "helloworldnumber" / pathVar[Int] / "foo" |>> { i: Int =>
+          Ok("Received $i")
+        }
         // the symbol 'world just says 'capture a String' with variable name "world"
-        GET / "helloworldstring" / 'world / "foo" |>> { i: String => Ok("Received $i") }
+        GET / "helloworldstring" / 'world / "foo" |>> { i: String =>
+          Ok("Received $i")
+        }
       }
       /// end_src_inlined
 
       /// src_inlined CaptureTail
       new RhoService {
         // You can capture the entire rest of the tail using *
-        GET / "hello" / * |>> { r: List[String] => Ok(s"Got the rest: ${r.mkString}") }
+        GET / "hello" / * |>> { r: List[String] =>
+          Ok(s"Got the rest: ${r.mkString}")
+        }
       }
       /// end_src_inlined
 
       /// src_inlined QueryCapture
       new RhoService {
         // Query parameters can be captured in a similar manner as path fragments
-        GET / "hello" +? param[Int]("fav") |>> { i: Int => Ok(s"Query 'fav' had Int value $i") } 
+        GET / "hello" +? param[Int]("fav") |>> { i: Int =>
+          Ok(s"Query 'fav' had Int value $i")
+        }
       }
       /// end_src_inlined
 
       /// src_inlined MultiCapture
       new RhoService {
         // A Path can be made all at once
-        POST / "hello" / pathVar[Int] +? param[Int]("fav") |>> { (i1: Int, i2: Int) =>
+        POST / pathVar[Int] +? param[Int]("fav") |>> { (i1: Int, i2: Int) =>
           Ok(s"Sum of the number is ${i1 + i2}")
         }
       }
@@ -80,7 +88,7 @@ class ApiExamples extends Specification {
         val getTag = capture(ETag)
 
         POST / "sequential" >>> getTag >>> ensureLength |>> { tag: ETag =>
-          Ok(s"Thanks for the $tag and the body with Content-Length greater than 0!")
+          Ok(s"Thanks for the $tag and the non-empty body!")
         }
       }
       /// end_src_inlined
@@ -89,8 +97,9 @@ class ApiExamples extends Specification {
       new RhoService {
         /*
          * Boolean logic
-         * Just as you can perform 'and' operations which have the effect of building up a path or
-         * making mutually required header validations, you can perform 'or' logic with your routes
+         * Just as you can perform 'and' operations which have the effect of
+         * building up a path or making mutually required header validations,
+         * you can perform 'or' logic with your routes.
          */
 
         val path1 = "one" / pathVar[Int]
@@ -99,7 +108,9 @@ class ApiExamples extends Specification {
         val getLength = captureMap(`Content-Length`)(_.length)
         val getTag = captureMap(ETag)(_ => -1)
 
-        GET / (path1 || path2) +? param[String]("foo") >>> (getLength || getTag) |>> {
+        GET / (path1 || path2) +?
+            param[String]("foo") >>>
+            (getLength || getTag) |>> {
           (i: Int, foo: String, v: Int) =>
             Ok(s"Received $i, $foo, $v")
         }
@@ -108,27 +119,36 @@ class ApiExamples extends Specification {
 
       /// src_inlined RequestAccess
       new RhoService {
-        // If you want to access the the Request, just add it as the first param
-        GET / "getrequest" |>> { req: Request => Ok("Dont need a request") }
-        GET / "getrequest" / 'foo |>> { (req: Request, foo: String) => Ok("I wanted a request") }
+        /* Access the `Request` by making it the first param of the
+           handler function.
+         */
+        GET / "request" |>> { req: Request =>
+          Ok("I don't acutally need a request...")
+        }
+        GET / "request" / 'foo |>> { (req: Request, foo: String) =>
+          Ok("I wanted a request")
+        }
       }
       /// end_src_inlined
 
       /// src_inlined ResultTypes
       new RhoService {
-        // Status codes are not mandatory: anything with an `EntityEncoder` will work.
+        private val counter = new AtomicInteger(0)
+        private def getCount(): String = counter.incrementAndGet().toString
+        // Don't want status codes? Anything with an `EntityEncoder` will work.
         GET / "nostatus" |>> { () => "No status!" }
-        GET / "taskNoStatus" |>> { () => Task("A Future status!") }
+        GET / "taskNoStatus" |>> { () => Task(getCount())
+        }
 
         /* Results need not be functions: they can be anything that has
            an `EntityEncoder` instance in scope */
         GET / "nostatus2" |>> "This is a constant result!"
-        GET / "taskNoStatus2" |>> Task("This task will be evaluated each time!")
+        GET / "taskNoStatus2" |>> Task(getCount())
 
         /* We can use a standard http4s.Response, but we don't get any metadata
            with it. Useful for things like Websocket support. */
         GET / "websockets" |>> { () =>
-          val exchange: scalaz.stream.Exchange[WebSocketFrame,WebSocketFrame] = ???
+          val exchange: stream.Exchange[WebSocketFrame,WebSocketFrame] = ???
           WS(exchange)
         }
       }
@@ -151,7 +171,6 @@ class ApiExamples extends Specification {
         }
       }
       /// end_src_inlined
-
 
       ok
     }
