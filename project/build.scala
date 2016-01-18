fc9ff308ecc3273b27a0da3f5a212ab40dcbd9b5
Bryce Anderson <bryce.anderson22@gmail.com>
2016-01-18 12:36:01 -0500

    Bump version to 0.10.0-SNAPSHOT

--- project/build.scala
+++ project/build.scala
@@ -21,7 +21,7 @@ object RhoBuild extends Build {
 
   val homepageUrl= "https://github.com/http4s/rho"
 
-  val rhoVersion = "0.9.0"
+  val rhoVersion = "0.10.0-SNAPSHOT"
 
   val apiVersion: (Int, Int) = extractApiVersion(rhoVersion)
 
