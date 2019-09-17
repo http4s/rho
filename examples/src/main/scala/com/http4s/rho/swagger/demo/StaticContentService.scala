7aaa8ec1f6b41a1f097305f25d2deb4ddc971b52
Rafał Sumisławski <rafal.sumislawski@gmail.com>
2019-09-17 20:20:51 +0200

    Use the * as a nicer syntax for CaptureTail.

--- examples/src/main/scala/com/http4s/rho/swagger/demo/StaticContentService.scala
+++ examples/src/main/scala/com/http4s/rho/swagger/demo/StaticContentService.scala
@@ -3,7 +3,6 @@ package com.http4s.rho.swagger.demo
 import cats.effect.{Blocker, ContextShift, IO}
 import org.http4s.dsl.io._
 import org.http4s.rho.RhoRoutes
-import org.http4s.rho.bits.PathAST.CaptureTail
 import org.http4s.{HttpRoutes, Request, Response, StaticFile}
 
 import scala.concurrent.ExecutionContext.global
@@ -21,9 +20,9 @@ object StaticContentService {
     */
   def routes(implicit cs: ContextShift[IO]): HttpRoutes[IO] = new RhoRoutes[IO] {
     // Swagger User Interface
-    GET / "css" / CaptureTail |>> { (req: Request[IO], _: List[String]) => fetchResource(swaggerUiDir + req.pathInfo, req) }
-    GET / "images" / CaptureTail |>> { (req: Request[IO], _: List[String]) => fetchResource(swaggerUiDir + req.pathInfo, req) }
-    GET / "lib" / CaptureTail |>> { (req: Request[IO], _: List[String]) => fetchResource(swaggerUiDir + req.pathInfo, req) }
+    GET / "css" / * |>> { (req: Request[IO], _: List[String]) => fetchResource(swaggerUiDir + req.pathInfo, req) }
+    GET / "images" / * |>> { (req: Request[IO], _: List[String]) => fetchResource(swaggerUiDir + req.pathInfo, req) }
+    GET / "lib" / * |>> { (req: Request[IO], _: List[String]) => fetchResource(swaggerUiDir + req.pathInfo, req) }
     GET / "swagger-ui" |>> { req: Request[IO] => fetchResource(swaggerUiDir + "/index.html", req) }
     GET / "swagger-ui.js" |>> { req: Request[IO] => fetchResource(swaggerUiDir + "/swagger-ui.min.js", req) }
   }.toRoutes()
